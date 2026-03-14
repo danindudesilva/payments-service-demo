9ab4369a3374e88d225df4300665fc3016b9e949
Danindu de Silva <danindu001@gmail.com>
2026-03-14 17:44:53 +0000

    feat: add payment not found error

--- internal/payments/domain/errors.go
+++ internal/payments/domain/errors.go
@@ -7,4 +7,5 @@ var (
 	ErrProviderNotLinked = errors.New("provider payment is not linked")
 	ErrInvalidMoney      = errors.New("invalid money")
 	ErrInvalidNextAction = errors.New("invalid next action")
+	ErrPaymentNotFound   = errors.New("payment attempt not found")
 )
